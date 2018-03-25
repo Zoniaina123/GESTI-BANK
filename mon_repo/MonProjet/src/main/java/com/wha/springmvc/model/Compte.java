fb818e36f5153be0fc4f9eef305e4773c1f49afa
youssef <yussuf9@gmail.com>
2018-03-25 22:43:17 +0200

    les mises a jour

--- mon_repo/MonProjet/src/main/java/com/wha/springmvc/model/Compte.java
+++ mon_repo/MonProjet/src/main/java/com/wha/springmvc/model/Compte.java
@@ -1,48 +1,112 @@
 package com.wha.springmvc.model;
 
 import java.io.Serializable;
-import java.sql.Date;
+import java.util.Collection;
+import java.util.Date;
 
-public class Compte implements Serializable {
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+
+@Entity
+@Table(name = "COMPTE")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type_compte", discriminatorType = DiscriminatorType.STRING, length = 3)
+
+public class Compte implements Serializable{
 	
 	private int numeroCompte;
 	 private double solde;
-    private Date dateCreation;
- 
-
-	public Compte() {
+     private Date dateCreation;
+   
+     @ManyToOne
+     @JoinColumn(name = "id_Client")
+   	private Client client;
+   	
+//   	@OneToMany(mappedBy = "compte")
+//	private Collection<Operation> operations;
+   	
+   
+   
+   
+   
+   
+public Compte() {
 
 	}
 
-	public Compte(Date dateCreation, double solde) {
 
-		this.dateCreation = dateCreation;
-		this.solde = solde;
-	}
+public Compte(double solde, Date dateCreation, Client client) {
+	super();
+	this.solde = solde;
+	this.dateCreation = dateCreation;
+	this.client = client;
+}
 
-	public int getNumeroCompte() {
-		return numeroCompte;
-	}
 
-	public void setNumeroCompte(int numeroCompte) {
-		this.numeroCompte = numeroCompte;
-	}
 
-	public Date getDateCreation() {
-		return dateCreation;
-	}
 
-	public void setDateCreation(Date dateCreation) {
-		this.dateCreation = dateCreation;
-	}
 
-	public double getSold() {
-		return solde;
-	}
 
-	public void setSolde(double solde) {
 
-		this.solde = solde;
-	}
+
+
+
+public int getNumeroCompte() {
+	return numeroCompte;
+}
+
+
+
+
+
+public void setNumeroCompte(int numeroCompte) {
+	this.numeroCompte = numeroCompte;
+}
+
+
+
+
+
+public double getSolde() {
+	return solde;
+}
+
+
+
+
+
+public void setSolde(double solde) {
+	this.solde = solde;
+}
+
+
+
+
+
+public Date getDateCreation() {
+	return dateCreation;
+}
+
+
+
+
+
+public void setDateCreation(Date dateCreation) {
+	this.dateCreation = dateCreation;
+}
+   
+   
+   
+   
 
 }
